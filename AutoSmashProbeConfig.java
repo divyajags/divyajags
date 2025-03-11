3ba7b46f556871bc85708e7a50ecd75e548cc348
divyajags <87128791+divyajags@users.noreply.github.com>
2025-03-11 00:00:43 -0700

    Update AutoSmashProbeConfig.java

--- AutoSmashProbeConfig.java
+++ AutoSmashProbeConfig.java
@@ -1,13 +1,4 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2761
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 HelveticaNeue;}
-{\colortbl;\red255\green255\blue255;\red236\green244\blue251;\red17\green21\blue26;}
-{\*\expandedcolortbl;;\cssrgb\c94118\c96471\c98824;\cssrgb\c8235\c10588\c13725;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
-
-\f0\fs32 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 package s3.index.probe.config;\
+ package s3.index.probe.config;\
 \
 import s3.configulator.Default;\
 import s3.configulator.DynamicallyConfigurable;\
